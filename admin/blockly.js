f8536996f1e4989b7edfd423dc655b08eb08232b
excal-foley <excal.foley@gmail.com>
2020-04-28 11:57:55 +0200

    move "checksPassThrough" to blockly.js and add importScripts into Blockly.BlocklyPlus

--- admin/blockly.js
+++ admin/blockly.js
@@ -4,24 +4,45 @@
  */
 'use strict';
 
-Blockly.Words['BLOCKLY-PLUS_obsolete']              = {'en': 'This block is obsolete and will be removed in future Blockly-Plus versions.',       'de': 'Dieser block ist veraltet und wird in zukünftigen Blockly-Plus Versionen entfernt.'    };
-Blockly.Words['BLOCKLY-PLUS_obsolete_block']        = {'en': 'Please replace it with the block: "$1"',                                            'de': 'Ersetzten sie diesen bitte durch den Block: "$1"'                                      };
-Blockly.Words['BLOCKLY-PLUS_obsolete_register']     = {'en': ' from the register: "$2"',                                                          'de': ' aus dem Register: "$2"'                                                               };
+Blockly.Words['BLOCKLY-PLUS_deprecated']              = {'en': '@deprecated. Will be deleted in future Blockly-Plus versions.',    'de': 'Dieser Block ist veraltet und wird in zukünftigen Blockly-Plus Versionen entfernt.'   };
+Blockly.Words['BLOCKLY-PLUS_deprecated_block']        = {'en': 'Use instead Block: $1',                                            'de': 'Verwenden Sie stattdessen den Block: $1'                                              };
 
-{
-// --- import scripts --------------------------------------------------
-  let isSandbox = /sandbox.html/.test(window.location.pathname);
-  let mainPath = isSandbox ? '../' : '../blockly-plus/';
-  let importScripts = function( srcList, addPath='' ) {
+// --- BlocklyPlus --------------------------------------------------
+Blockly.BlocklyPlus = {
+
+  isSandbox: /sandbox.html/.test(window.location.pathname),
+
+  get mainPath() { return this.isSandbox ? '../' : '../blockly-plus/' },
+
+  markDeprecated: function(block, alternateBlock) {
+    let warnText = Blockly.Words['BLOCKLY-PLUS_deprecated'][systemLang];
+    if (alternateBlock) warnText += '\n'+
+        Blockly.Words['BLOCKLY-PLUS_deprecated_block'][systemLang]
+        .replace('$1', alternateBlock);
+    block.setWarningText(warnText);
+  },
+
+  Marker: function(block) {
+    let field = new Blockly.FieldImage(this.mainPath + 'blockly-plus.png', 15, 15, '➕');
+    block.inputList[0].insertFieldAt(0, field, 'Blockly-Plus_MARKER');
+  },
+
+  importScripts: function(srcList, addPath='', addMainPath=false) {
     !Array.isArray(srcList) && (srcList = [srcList]);
-    srcList.forEach(function(src) {
+    srcList.forEach((src) => {
       var script = document.createElement('script');
       script.async = false; // Force synchronous loading, to load them in the correct sequence
-      script.src = addPath + src;
+      script.src = (addMainPath ? this.mainPath : '') + addPath + src;
       document.head.appendChild(script);
     });
   }
 
+  //getWord: (name) => Blockly.Words['Blockly-Plus_'+name][systemLang]
+
+};
+
+// --- block import --------------------------------------------------
+{
   let importList = {};
 
   importList.blockly = [
@@ -44,28 +65,78 @@ Blockly.Words['BLOCKLY-PLUS_obsolete_register']     = {'en': ' from the register
   ];
   // DO NOT EDIT THIS LINE!!! ################################
 
-  importList.blocks.push('old_own_code.js');
+  Blockly.BlocklyPlus.importScripts(importList.blockly, 'blockly/', true);
+  Blockly.BlocklyPlus.importScripts(importList.blocks, 'blockly/blocks/', true);
 
-  importScripts(importList.blockly, mainPath + 'blockly/');
-  importScripts(importList.blocks, mainPath + 'blockly/blocks/');
+  if (Blockly.BlocklyPlus.isSandbox) Blockly.BlocklyPlus.importScripts('makeToolbox.js');
 
-  if (isSandbox) importScripts('makeToolbox.js');
+}
 
-// --- BlocklyPlus --------------------------------------------------
-  Blockly.BlocklyPlus = {
-    Obsolete: function(block, alternateBlock, alternateRegister) {
-      let warnText = Blockly.Words['BLOCKLY-PLUS_obsolete'][systemLang];
-      if (alternateBlock) warnText += '\n'+
-          Blockly.Words['BLOCKLY-PLUS_obsolete_block'][systemLang]
-          .replace('$1', alternateBlock);
-      if (alternateRegister) warnText +=
-          Blockly.Words['BLOCKLY-PLUS_obsolete_register'][systemLang]
-          .replace('$2', alternateRegister);
-      block.setWarningText(warnText);
-    },
-    Marker: function(block) {
-      let field = new Blockly.FieldImage(mainPath + 'blockly-plus.png', 15, 15, '➕');
-      block.inputList[0].insertFieldAt(0, field);
+// --- checksPassThrough --------------------------------------------------
+/**                | out2in | in2out |
+ *   in<>in        |    0   |    0   |
+ *       in <- out |    1   |    0   |
+ *   in<>in -> out |    0   |    1   |
+ *   in<>in <> out |    1   |    1   |
+ * This function belongs in an onchange-method of a block.
+ * It changes the input/output-checks depending on the connected blocks.
+ * This is triggered when connecting to other blocks or when creating/rebuilding.
+ * @param  {BlockSvg}   block               Blockly.BlockSvg
+ * @param  {Events}     event               Blockly.Events
+ * @param  {Boolean}    [in2out=true]       Should the inputs-checks be transferred to the output
+ * @param  {Boolean}    [out2in=true]       Should the output-checks be transferred to the inputs
+ * @param  {?string[]}  [selectInputs=null] names of inputs which to take into account
+ *                                          If null then all inputs are considered
+ */
+Blockly.Blockly.checksPassThrough = function(block, event, in2out = true, out2in = true, selectInputs = null) {
+  let blockCreate = event.type == 'create' && ( event.blockId == block.id ||
+                                                event.ids.includes(block.id) );
+  let blockPlugged = event.type == 'move' && ( event.blockId == block.id &&
+                                        event.newParentId != event.oldParentId);
+  let blockConnect = event.type == 'move' && ( event.newParentId == block.id ||
+                                               event.oldParentId == block.id );
+
+  if (blockCreate || blockPlugged || blockConnect) {
+
+    let checks = new function() {
+      this.inputs = null;
+      this.output = block.outputConnection.targetConnection
+                          && block.outputConnection.targetConnection.check_;
+      this.out2in = () => { this.inputs = this.output };
+      this.in2out = () => { this.output = this.inputs };
+      this.addInput = function(check) {
+        if (check) {
+          if (!this.inputs) {
+            this.inputs = check;
+          } else {
+            this.inputs = this.inputs.filter(n => check.includes(n));
+          }
+        }
+      };
+      this.getInputs = function() {
+        for (let input of block.inputList) {
+          if (selectInputs && !selectInputs.includes(input.name)) continue;
+          let connectBlock = input.connection.targetBlock();
+          if (event.oldInputName != input.name && connectBlock) {
+            let connectBlockCheck = connectBlock.outputConnection.check_;
+             checks.addInput(connectBlockCheck);
+          }
+        }
+      };
     }
-  };
+
+    if (out2in && !in2out) checks.out2in();
+    else checks.getInputs();
+
+    if (out2in) checks.addInput(checks.output);
+    if (in2out) checks.in2out();
+
+    // set checks in all inputs
+    for (let input of block.inputList) {
+      if (selectInputs && !selectInputs.includes(input.name)) continue;
+      input.setCheck(checks.inputs);
+    }
+    // set output check
+    block.setOutput(true, checks.output);
+  }
 }
